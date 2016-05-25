a70182a41f0f8e0a4e345d5e1eb9674eae42531c
Daniel Micaletti <danielmicaletti@gmail.com>
2016-05-24 21:11:27 -0700

    remove more sensitive files

--- student_portal/email_info.py
+++
@@ -1,8 +0,0 @@
-# For gmail and google apps
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_HOST= 'smtp.gmail.com'
-EMAIL_HOST_USER = 'hgatestacct@gmail.com'
-EMAIL_HOST_PASSWORD = 'HirschGuitarAcademy2016'
-EMAIL_PORT = 587
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- student_portal/wsgi.py
+++ student_portal/wsgi.py
@@ -14,9 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "student_portal.settings")
 
 application = get_wsgi_application()
-
-
-SECRET_KEY = 'tr4ocp+b=@q)sl3i8ri9f(*n#yf#tk3*n-b+_-tw^9u^0bbiv3'
-AWS_STORAGE_BUCKET_NAME = 'hirschguitar'
-AWS_ACCESS_KEY_ID = 'AKIAIVTWRJ5XEPGQRVZQ'
-AWS_SECRET_ACCESS_KEY = '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'
