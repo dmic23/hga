1fce07f47f18dfeaefe6389673ddaa8df098d30a
Daniel Micaletti <dmic23@gmail.com>
2016-06-11 12:20:37 -0700

    Update email_info.py

--- student_portal/email_info.py
+++ student_portal/email_info.py
@@ -2,7 +2,5 @@
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST= 'smtp.gmail.com'
-EMAIL_HOST_USER = 'hgatestacct@gmail.com'
-EMAIL_HOST_PASSWORD = 'HirschGuitarAcademy201623'
 EMAIL_PORT = 587
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
